41b17eb79b8d87d2d673835cdcf5ca2c73c2c980
Nitin Varma <nitinvarma@nitins-air.wifi.uc.edu>
2022-09-16 11:45:36 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 100.0;
+        double summerCost = 75.59;
+        double fallCost = 105.49;
+        double winterCost = 99.99;
+
+        totalYearlyMaintenanceCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("The total yearly maintenance cost is: ") + totalYearlyMaintenanceCost = springCost + summerCost + fallCost + winterCost;
+
+    }
+}
